83f71335d9c6936390d74e872b1bd288947bac57
HassanAlkhawam <halkhawam01@qub.ac.uk>
2024-02-19 16:33:49 +0000

    connecting frontend with the backend for Login and Register pages. geting the values of the users and keep it so we can use it in the future. once register or login completed it will take user to the main menu

--- chronokill-next-app/app/(pages)/login/page.tsx
+++ chronokill-next-app/app/(pages)/login/page.tsx
@@ -1,35 +1,80 @@
-"use client"
-import styles from './styles.module.css'
-import { Button, TextInput, Header } from '../../components'
-import { useState } from 'react'
+"use client";
+
+import styles from "./styles.module.css";
+import { Button, TextInput, Header } from "../../components";
+import { useState } from "react";
 
 export const Login = () => {
-	const [password, setPassword] = useState("")
-	const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-	const onClickLog = (e: any ) => {
-		e.preventDefault();
-		console.log(email, password )
-	}
+  const onClickLog = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log(email, password)
+    
+    const userCredentials = {
+      email: email,
+      password: password,
+    };
+  console.log(JSON.stringify(userCredentials));
 
-	return (
-	<div>
-		<div className={styles.bg}></div>
-		<Header showBackButton={false} />
-		<div className={styles.rectangle}>
-			<h1 className={styles.h1}>Login</h1>
-			<form>
-				<TextInput type="email" title="Email" value={email} setter={setEmail}/>
-				<TextInput type="password" title="Password" value={password} setter={setPassword}/>
-				<Button onClick={onClickLog}>Login</Button>
-				<hr className={styles.hr}></hr>
-				<p>No Account?</p>
-				<Button href="../register"> Register</Button>
-				
-			</form>
-		</div>
-	</div>
-	)
-}
+   fetch("http://localhost:8080/api/login", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userCredentials),
+  })
+      .then((response) => {
+          if (response.ok) {
+              return response.text(); 
+          } else {
+              console.error("Login failed");
+              
+          }
+      })
+      .then((data) => {
+          console.log("Login successful:", data);
+          window.location.href ='/mainmenu';
+          
+      })
+      .catch((error) => {
+          console.error("Error during Login:", error);
+      })
+      .finally(() => {
+          console.log(email,password);
+      });
   
-export default Login
+  
+	  };
+
+  return (
+    <div>
+      <div className={styles.bg}></div>
+      <Header showBackButton={false} />
+      <div className={styles.rectangle}>
+        <h1 className={styles.h1}>Login</h1>
+        <form>
+          <TextInput
+            type="email"
+            title="Email"
+            value={email}
+            setter={setEmail}
+          />
+          <TextInput
+            type="password"
+            title="Password"
+            value={password}
+            setter={setPassword}
+          />
+          <Button onClick={onClickLog}>Login</Button>
+          <hr className={styles.hr}></hr>
+          <p>No Account?</p>
+          <Button href="../register"> Register</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- chronokill-spring-app/src/main/java/qub/chronokill/springapp/entity/repository/UserRepository.java
+++ chronokill-spring-app/src/main/java/qub/chronokill/springapp/entity/repository/UserRepository.java
@@ -3,5 +3,8 @@ package qub.chronokill.springapp.entity.repository;
 import qub.chronokill.springapp.entity.model.Users;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<Users,Integer> {
+    Optional<Users> findByEmail(String email);
 }
