31d373846430fe16182434b68f874a95939acb0b
John Pinkerton <jpinkerton03@qub.ac.uk>
2024-03-19 17:24:43 +0000

    TWC-70: Update Top Bar

--- chronokill-next-app/app/components/TopPanel/TopPanel.tsx
+++ chronokill-next-app/app/components/TopPanel/TopPanel.tsx
@@ -6,11 +6,10 @@ export const TopPanel = () => {
 
   return (
     <div className={styles.topPanel}>
-      <div className={styles.dateWrapper}>Monday, 22 / 01 / 24, 09:00</div>
-      <div className={styles.titleWrapper}>ChronoKill</div>
+      <div className={styles.dateWrapper}></div>
+      <div className={styles.titleWrapper}>Chronokill</div>
       <div className={styles.burgerMenuArea}>
         <Link href="/">Home</Link>
-        <Link href="/login">Login</Link>
       </div>
     </div>
   )
